e53820cf8e75fe7bd82e4a31800161cd306b1b43
rahul <r.rahul@bidpath.com>
2024-03-27 18:47:51 +0530

    created config file with base URL and browser options

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,9 +25,11 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://qa-dev-auto.auctionserver.net/                                                                                                                    ',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
